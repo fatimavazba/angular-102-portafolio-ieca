7be02b2fe540f2e6c8ac6a3b97baaf481edc0bdd
Fátima Berenice Vázquez Barrientos <fatima.vazquez@utng.edu.mx>
2024-03-25 14:27:17 -0600

    subiendo cambios de 4-angular

--- 4-angular/src/app/app.component.spec.ts
+++ 4-angular/src/app/app.component.spec.ts
@@ -26,4 +26,8 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fundamentos-ag');
   });
+
+  limpiar(){
+    this.passkey = ' ';  }
+
 });
